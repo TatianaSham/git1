adc0868e25e066b65b72cd04f24be2433fa0eaa8
TatianaSham <156084120+TatianaSham@users.noreply.github.com>
2024-11-27 21:45:32 +0300

    Update program.py
    add

--- program.py
+++ program.py
@@ -1,6 +1,9 @@
 def main():
     print('My ___first git program')
     print('And I change it every day')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
 
 if __name__ == '__main__':
     main()
